c322bf5639e6d5a2f1b88acff4ac46ea3862a44b
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2022-03-13 06:24:04 -0300

    Add project files.

---
+++ AcessoDados.Balta/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace AcessoDados.Balta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+            //Microsoft.Data.SqlClient == pacote para se conectar ao Banco de dados
+            
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                
+                using(var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] From [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }
+
+        }
+    }
+}
