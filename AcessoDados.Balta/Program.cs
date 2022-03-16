b84ed117bd9313e904377185e7f5ade9b10c63b1
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2022-03-16 09:07:12 -0300

    Codigo Melhorado com Dapper

--- AcessoDados.Balta/Program.cs
+++ AcessoDados.Balta/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AcessoDados.Balta.Models;
+using Dapper;
 using Microsoft.Data.SqlClient;
+
+//Microsoft.Data.SqlClient == pacote para se conectar ao Banco de dados
+// Dapper é uma extensão a Microsoft.Data.SqlClient
 
 namespace AcessoDados.Balta
 {
@@ -12,23 +17,43 @@ namespace AcessoDados.Balta
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=false";
-            //Microsoft.Data.SqlClient == pacote para se conectar ao Banco de dados
-            
-            using(var connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                
-                using(var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] From [Category]";
 
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                    }
+            //Inserção com Dapper Parte 1 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Featured = false;
+            
+
+            /*NÃO DEVEMOS CONCATENAR STRING ""$ + {0}"" EM INSERT UPDATE, SELECT...
+              PARA EVITAR SQLINJECTION*/
+            var insertSql = @"INSERT INTO [Category] 
+                VALUES(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                //Inserção com Dapper Parte 2
+                var rows = connection.Execute(insertSql, new { 
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"{rows} linhas inseridas");
+
+                //Consulta com Dapper
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
 
