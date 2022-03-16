8f23a862185117beb34ce3742803511b8b9f0a5a
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2022-03-16 09:20:36 -0300

    Add Metodo Update com Dapper

--- AcessoDados.Balta/Program.cs
+++ AcessoDados.Balta/Program.cs
@@ -8,7 +8,7 @@ using Dapper;
 using Microsoft.Data.SqlClient;
 
 //Microsoft.Data.SqlClient == pacote para se conectar ao Banco de dados
-// Dapper é uma extensão a Microsoft.Data.SqlClient
+//Dapper é uma extensão a Microsoft.Data.SqlClient
 
 namespace AcessoDados.Balta
 {
@@ -17,6 +17,30 @@ namespace AcessoDados.Balta
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=false";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                UpdateCategory(connection);
+                ListCategories(connection);
+                //CreateCategories(connection);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            //Consulta com Dapper
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void CreateCategories(SqlConnection connection)
+        {
 
             //Inserção com Dapper Parte 1 
             var category = new Category();
@@ -27,37 +51,37 @@ namespace AcessoDados.Balta
             category.Order = 8;
             category.Description = "Categoria destinada a serviços do AWS";
             category.Featured = false;
-            
 
             /*NÃO DEVEMOS CONCATENAR STRING ""$ + {0}"" EM INSERT UPDATE, SELECT...
               PARA EVITAR SQLINJECTION*/
             var insertSql = @"INSERT INTO [Category] 
                 VALUES(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
-
-            using (var connection = new SqlConnection(connectionString))
+           
+            //Inserção com Dapper Parte 2
+            var rows = connection.Execute(insertSql, new
             {
-                //Inserção com Dapper Parte 2
-                var rows = connection.Execute(insertSql, new { 
-                    category.Id,
-                    category.Title,
-                    category.Url,
-                    category.Summary,
-                    category.Order,
-                    category.Description,
-                    category.Featured
-                });
-                Console.WriteLine($"{rows} linhas inseridas");
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
 
-                //Consulta com Dapper
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+        //Atualização de Registro com Dapper
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@Title Where [Id]=@Id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                Id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                Title = "Frontend 2021"
+            });
 
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
-            }
-
-            Console.ReadKey();
+            Console.WriteLine($"{rows} registros atualizados");
         }
     }
 }
